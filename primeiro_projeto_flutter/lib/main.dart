8fd71a9a4e371964af620676412883ced9a11e69
Julianna Mariano <juliannamariano27@outlook.com>
2025-04-14 16:06:29 -0300

    Implementar layout utilizando StatefulWidget

--- primeiro_projeto_flutter/lib/main.dart
+++ primeiro_projeto_flutter/lib/main.dart
@@ -3,47 +3,89 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
-/*STACK = pilha, elementps um sobre o outro*/
+//Com esse widget NÃO ocorre mudanças dentro do app, pois ele é estático
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  /*MaterialApp: configura o app com o estilo do Material Design.
-theme: define as cores e estilo geral do app.
-home: é a tela inicial do app — nesse caso, um Stack.*/
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         //widget do material design. Ele pede alguns parâmetros: TOPO, MEIO, FUNDO
         appBar: AppBar(title: Text('Tarefas')),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(color: Colors.blue, height: 140),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(color: Colors.black26, width: 72, height: 100),
-                    Text('Aprender flutter'),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Icon(Icons.arrow_drop_up),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
+        body: ListView(
+          children: [
+            Task(nome: 'Num 1'),
+            Task(nome: 'Num 2'),
+            Task(nome: 'Num 3'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Com esse widget ocorre mudanças dentro do app
+class Task extends StatefulWidget {
+  final String nome;
+
+  const Task({super.key, required this.nome});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+  @override
+
+
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(color: Colors.blue, height: 140),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(color: Colors.black26, width: 72, height: 100),
+                      Container(
+                        width: 200,
+                        child: Text(
+                          widget.nome,
+                          style: TextStyle(fontSize: 24),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nivel++;
+                          });
+
+                          print(nivel);
+                        },
+                        child: Icon(Icons.arrow_drop_up),
+                      ),
+                    ],
+                  ),
+                ),
+                Text('Nivel $nivel'),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
